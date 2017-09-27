fab2eb03ba8363f610214cdf997b973fa5a833c9
Paul Decoursey <paul@decoursey.net>
2017-09-27 11:36:42 -0500

    API changes

--- src/index.js
+++ src/index.js
@@ -9,18 +9,25 @@ function getNode(type, id) {
     return base.getObject(tableName.call({type: type}), id);
 }
 
-function findNodeWithAttribute(type, attr) {
+function listNodes(type, token) {
+    return {"nodes": [], "token": null};
+}
+
+function findNodesWithAttribute(type, attr, token) {
+    // TODO - add token to base call and response
     return base.getSubjectsWithPredicate(tableName.call({type: type}), attr);
 }
 
-function findNodeWithSpecificAttribute(type, attr, value) {
+function filterNodes(type, attr, value, token) {
+    // TODO - add token to base call and response
     return base.getSubjectsWithPredicateValue(tableName.call({type: type}), attr, value);
 }
 
 
 exports.getNode = getNode;
-exports.findNodeWithAttribute = findNodeWithAttribute;
-exports.findNodeWithSpecificAttribute = findNodeWithSpecificAttribute;
+exports.listNodes = listNodes;
+exports.findNodesWithAttribute = findNodesWithAttribute;
+exports.filterNodes = filterNodes;
 
 
 exports.deploy = function(project, tables, environment) {
