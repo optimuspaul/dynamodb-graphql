3c5f72f6c75d3923f511b52cdbdeada1700d4da1
Kevin M. Truckenmiller <ktruckenmiller@gmail.com>
2017-12-01 10:58:22 -0600

    updated tests

--- src/base.js
+++ src/base.js
@@ -208,6 +208,7 @@ function removeObject(tableName, subject) {
     return new bluebird.Promise(function(resolve, reject) {
         getObjectTriples(tableName, subject)
             .then(function(data) {
+              
                 data.forEach(function(triple) {
                     ops.push({
                         DeleteRequest: {
