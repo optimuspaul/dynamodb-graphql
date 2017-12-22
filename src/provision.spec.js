179537c1272a4248d73ae862f9a86106b0e323ed
Kevin M. Truckenmiller <ktruckenmiller@gmail.com>
2017-12-22 10:40:34 -0600

    updated to autoscale

--- src/provision.spec.js
+++ src/provision.spec.js
@@ -1,6 +1,7 @@
-const provision = require('./provision')  
+const provision = require('./provision')
 const expect = require('chai').expect
-
+const AWS = require('aws-sdk')
+const cloudformation = new AWS.CloudFormation()
 
 //--------------------------------------------------
 // Tests that the basic API contract is maintained.
@@ -11,7 +12,7 @@ const functions = [
 
 
 functions.forEach(function(func_name) {
-    describe('provision module api', function() {  
+    describe('provision module api', function() {
       describe('"'+func_name+'"', function() {
         it('should export a function', function() {
           expect(provision[func_name]).to.be.a('function');
@@ -24,12 +25,51 @@ functions.forEach(function(func_name) {
 //--------------------------------------------------
 // Tests the generateCFN generates what we expect.
 //--------------------------------------------------
-describe('generateCFN module functionality', function() {  
+describe('generateCFN module functionality', function() {
     describe('"generateCFN"', function() {
-        it('return a YML formatted cloudformation template with two tables defined and one IAM role', function() {
-            var cfn = provision.generateCFN("unit-tests", ["one", "hat"]);
+        it('return a YML formatted cloudformation template with two tables defined and one IAM role', function(done) {
+          this.timeout(15000)
+            const extraParams = [
+            	{name: "Unit", type: "String"},
+            	{name: "Product", type: "String"},
+            	{name: "Subproduct", type: "String"},
+            	{name: "Version", type: "String"},
+            ]
+            var cfn = provision.generateCFN("UnitTests", ["one", "hat"], extraParams);
             // console.log(cfn);
+            cloudformation.validateTemplate({
+              TemplateBody: cfn
+            }, function(err, data) {
+              expect(err).to.be.null;
+              // cloudformation.createStack({
+              //   StackName: "int-test-dynamodb-graphql",
+              //   OnFailure: "DELETE",
+              //   TemplateBody: cfn,
+              //   Parameters: [{
+              //     ParameterKey: 'Unit',
+              //     ParameterValue: "test",
+              //     UsePreviousValue: false
+              //   },{
+              //     ParameterKey: 'Product',
+              //     ParameterValue: "test",
+              //     UsePreviousValue: false
+              //   },{
+              //     ParameterKey: 'Subproduct',
+              //     ParameterValue: "test",
+              //     UsePreviousValue: false
+              //   },{
+              //     ParameterKey: 'Version',
+              //     ParameterValue: "test",
+              //     UsePreviousValue: false
+              //   }],
+              //   Capabilities: ["CAPABILITY_NAMED_IAM"]
+              // }, function(err, data) {
+              //   console.log(err, data)
+              //   done()
+              // })
+
+            })
+
         });
     });
 });
-
